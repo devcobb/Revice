cb91bf8584969527f2a2a71a83ae265e4d119a7a
Jakub <devcobbier@gmail.com>
2022-01-02 13:56:38 +0100

    Seperated menu to different component, finished mobile offcanvas menu, some responsive fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { MenuComponent } from './components/page-header/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
