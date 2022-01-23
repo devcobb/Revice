9cf135d88e3552b259665e2f22bc852047b1dac9
Jakub <devcobbier@gmail.com>
2022-01-23 14:26:01 +0100

    Added firebase authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddNewPostComponent } from './components/add-new-post/add-new-post.component';
 import { HeroImageComponent } from './components/hero-image/hero-image.component';
+import { LoginViewComponent } from './components/login-view/login-view.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./global/auth/auth.module').then((m) => m.AuthModule),
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HeroImageComponent },
-  { path: 'new', component: AddNewPostComponent }
+  { path: 'new', component: AddNewPostComponent },
+  { path: 'login', component: LoginViewComponent },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddNewPostFormComponent } from './components/add-new-post/add-new-post-form/add-new-post-form.component';
 import { AddNewPostComponent } from './components/add-new-post/add-new-post.component';
 import { CategoryListElementComponent } from './components/add-new-post/category-list-element/category-list-element.component';
+import { UploadImageComponent } from './components/add-new-post/upload-image/upload-image.component';
 import { HeroImageComponent } from './components/hero-image/hero-image.component';
+import { LoginViewComponent } from './components/login-view/login-view.component';
 import { MenuComponent } from './components/page-header/menu/menu.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
-import { AddNewPostFormComponent } from './components/add-new-post/add-new-post-form/add-new-post-form.component';
-import { UploadImageComponent } from './components/add-new-post/upload-image/upload-image.component';
-
 
 @NgModule({
   declarations: [
@@ -20,11 +24,15 @@ import { UploadImageComponent } from './components/add-new-post/upload-image/upl
     AddNewPostComponent,
     CategoryListElementComponent,
     AddNewPostFormComponent,
-    UploadImageComponent
+    UploadImageComponent,
+    LoginViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
