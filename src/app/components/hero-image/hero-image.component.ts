3424c8a3f28d5afa3fc7a7463638c6242778ff69
Jakub <devcobbier@gmail.com>
2022-03-11 18:14:02 +0100

    Reworked hero image and some scss clean up

--- src/app/components/hero-image/hero-image.component.ts
+++ src/app/components/hero-image/hero-image.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-image',
   templateUrl: './hero-image.component.html',
   styleUrls: ['./hero-image.component.scss']
 })
-export class HeroImageComponent {}
+export class HeroImageComponent {
+  @Input() height: string = "100vh";
+  @Input() imageID: number = 1;
+  @Input() content: boolean = true;
+  @Input() wave: boolean = false;
+}
